bf067188ca5162b33d0d7704e8de1b62adf45f9a
Jakub Piotr Cłapa <jpc@loee.pl>
2013-02-19 14:36:57 +0100

    NFY: maxbaud for LPC17xx.

--- native-code/lpc17xx/dbg.c
+++ native-code/lpc17xx/dbg.c
@@ -19,13 +19,20 @@ void map_boot () {
 #define U0FDR U0REG(0x28)
 #define PCLKSEL0 (*(volatile unsigned int *)(0x400FC1A8))
 
+#define PLL0REG(x) (*(volatile unsigned int *)((x) + 0x400fC080))
+#define PLL0CON  PLL0REG(0x00)
+#define PLL0CFG  PLL0REG(0x04)
+#define PLL0STAT PLL0REG(0x08)
+#define PLL0FEED PLL0REF(0x0C)
+
 void baud_max () {
   while (!(U0LSR & (1 << 6)));
   PCLKSEL0 |= 1 << 6;
-  U0FDR = (13 << 4) | 3;
+  // FIXME: SETUP PLL for 12MHz from 4MHz IRC
+  U0FDR = (1 << 4) | 0; // disable fractional
   U0LCR |= 1 << 7; // DLAB = 1
   U0DLM = 0;
-  U0DLL = 3;
+  U0DLL = 1; // 12MHz / 16 / 1 = 750000
   U0LCR &= ~(1 << 7); // DLAB = 0
   U0FCR = 0x7; // reset fifos
 }
